7473c5e629c6de59db03ebbf5fc25f0142ae4cb7
gretzkiy <dias7707@gmail.com>
2021-10-20 20:52:14 +0300

    work

--- src/TestVersFirst.java
+++ src/TestVersFirst.java
@@ -5,31 +5,31 @@ public class TestVersFirst {
     public static void main(String[] args) {
 
         //Первоначальный массив
-        String matrixStr = "4 1\n-1 1";
-        ArrayList<ArrayList<Integer>> matrixMain = parseMatrixFromString(matrixStr);
+        String matrixStr = "3 4 2\n4 3 3";
+        ArrayList<ArrayList<Double>> matrixMain = parseMatrixFromString(matrixStr);
         int yCount = matrixMain.size();
 
         //Значения равенств уравнений
-        String resultStr = "8 3";
-        ArrayList<Integer> result = parseArrayFromString(resultStr);
-        ArrayList<ArrayList<Integer>> fullMatrix = new ArrayList<ArrayList<Integer>>();
+        String resultStr = "1020 940";
+        ArrayList<Double> result = parseArrayFromString(resultStr);
+        ArrayList<ArrayList<Double>> fullMatrix = new ArrayList<ArrayList<Double>>();
 
         //Заполнение матрицы дополнительными переменными
         for (int i = 0; i < yCount; i++) {
-            ArrayList<Integer> fullRow = (ArrayList<Integer>) matrixMain.get(i).clone();
+            ArrayList<Double> fullRow = (ArrayList<Double>) matrixMain.get(i).clone();
             for (int j = 0; j < yCount; j++) {
                 if (j == i) {
-                    fullRow.add(1);
+                    fullRow.add(1.0);
                 } else {
-                    fullRow.add(0);
+                    fullRow.add(0.0);
                 }
             }
             fullMatrix.add(fullRow);//заполнение матрицы дополнительными переменными
         }
 
         //Значения целевой функции
-        String targetFuncStr = "3 4";
-        ArrayList<Integer> targetFunc = parseArrayFromString(targetFuncStr);
+        String targetFuncStr = "10 12 8";
+        ArrayList<Double> targetFunc = parseArrayFromString(targetFuncStr);
 
         //Значения целевой функции умножаем на -1
         for (int i = 0; i < targetFunc.size(); i++) {
@@ -38,15 +38,15 @@ public class TestVersFirst {
 
         //Добавляем к целевой функции дополнительные переменные
         for (int i = 0; i < yCount; i++) {
-            targetFunc.add(0);
+            targetFunc.add(0.0);
         }
 
         //Инициализация массива, где буду храниться базисы
-        ArrayList<Integer> basis = new ArrayList<>();
+        ArrayList<Double> basis = new ArrayList<>();
 
         //Из targetFunc находим наименьшее число
         //В indexOfColumn записывается индекс разрешающего столбца
-        int min = 100000000;
+        Double min = 10000.0;
         int indexOfColumn = 10000000;
         for (int i = 0; i < targetFunc.size(); i++) {
             if (targetFunc.get(i) < min) {
@@ -62,8 +62,8 @@ public class TestVersFirst {
 
         //Находим минимальное число из базиса (если число =0 или < 0 его не учитываем)
         //В indexOfRow записывается индекс разрешающего столбца
-        int minOfBasis = 100000000;
-        int indexOfRow = 1000000000;
+        Double minOfBasis = 1000.0;
+        Integer indexOfRow = 1000000000;
         for (int i = 0; i < basis.size(); i++) {
             if (basis.get(i) > 0) {
                 if (basis.get(i) < minOfBasis) {
@@ -73,43 +73,63 @@ public class TestVersFirst {
             }
         }
 
+
+        double elemenOfRowColum = fullMatrix.get(indexOfRow).get(indexOfColumn);
         //Очищаем базис, так как он будет высчитываться снова
         basis.clear();
 
         //Перезаполняем матрицу с дополнительными переменными новыми значениями
 
         //для разрешающей строки делаем вычисления - каждый элемент разрешающей строки делим на разрешающий элемент -fullMatrix.get(indexOfRow).get(indexOfColumn)
-        ArrayList<Integer> fullRow = fullMatrix.get(indexOfRow);
+        ArrayList<Double> fullRow = fullMatrix.get(indexOfRow);
         //Добавить в расчет данные из result
-        fullRow.add(result.get(indexOfColumn));
+        fullRow.add(result.get(indexOfRow));
+
         for (int i = 0; i < fullRow.size(); i++) {
-            fullRow.set(i, fullRow.get(i) / fullMatrix.get(indexOfRow).get(indexOfColumn));
+            fullRow.set(i, fullRow.get(i) / elemenOfRowColum);
         }
 
         //Вернем последний элемент в массив result и удалим его из fullRow
-        result.set(indexOfColumn, fullRow.get(fullRow.size()-1));
+        result.set(indexOfRow, fullRow.get(fullRow.size()-1));
         fullRow.remove(fullRow.size() -1);
 
         //Перезаписываем в главной матрице с доп переменными разрешающую строку новыми значениями
         fullMatrix.set(indexOfRow, fullRow);
+        System.out.println(fullRow);
 
-        Integer newResult = 0;
-        for (int i = 0; i < fullMatrix.size(); i++) {
-            ArrayList<Integer> fillRow = new ArrayList<>();
-            if (i != indexOfRow) {
-                for (int j = 0; j < fullMatrix.get(i).size(); j++) {
-                    fillRow.add(fullRow.get(j) * fullMatrix.get(i).get(indexOfColumn) * -1 + fullMatrix.get(i).get(j));
+        for (int i = 0; i < fullMatrix.size(); i++){
+            ArrayList<Double> fillRow = new ArrayList<>();
+            if (i != indexOfRow){
+                for (int j = 0; j < fullMatrix.get(i).size(); j++){
+                    fillRow.add((fullRow.get(j) * fullMatrix.get(i).get(indexOfColumn) * -1) +fullMatrix.get(i).get(j));
                 }
-                fullMatrix.set(i, fillRow);
+                result.set(i,(result.get(indexOfRow) * fullMatrix.get(i).get(indexOfColumn) * -1) + result.get(i));
+                fullMatrix.set(i,fillRow);
             }
         }
-
-
+        
         System.out.println(fullMatrix);
-        System.out.println(fullRow);
-        System.out.println(targetFunc);
-
+        System.out.println(result);
     }
+
+
+    public static ArrayList<ArrayList<Double>> addAdditionalElements(int yCount, ArrayList<ArrayList<Double>> matrixMain){
+        ArrayList<ArrayList<Double>> fullMatrix = new ArrayList<ArrayList<Double>>();
+        //Заполнение матрицы дополнительными переменными
+        for (int i = 0; i < yCount; i++) {
+            ArrayList<Double> fullRow = (ArrayList<Double>) matrixMain.get(i).clone();
+            for (int j = 0; j < yCount; j++) {
+                if (j == i) {
+                    fullRow.add(1.0);
+                } else {
+                    fullRow.add(0.0);
+                }
+            }
+            fullMatrix.add(fullRow);//заполнение матрицы дополнительными переменными
+        }
+        return fullMatrix;
+    }
+
 
     /**
      * Метод для парсинга введенных строк в матрицу
@@ -117,9 +137,9 @@ public class TestVersFirst {
      * @param matrixStr
      * @return
      */
-    private static ArrayList<ArrayList<Integer>> parseMatrixFromString(String matrixStr) {
+    private static ArrayList<ArrayList<Double>> parseMatrixFromString(String matrixStr) {
         String[] matrixRowsStr = matrixStr.split("\n");
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
 
         for (String matrixRowStr : matrixRowsStr) {
             matrix.add(parseArrayFromString(matrixRowStr));
@@ -134,12 +154,12 @@ public class TestVersFirst {
      * @param arrayStr
      * @return
      */
-    private static ArrayList<Integer> parseArrayFromString(String arrayStr) {
-        ArrayList<Integer> array = new ArrayList<>();
+    private static ArrayList<Double> parseArrayFromString(String arrayStr) {
+        ArrayList<Double> array = new ArrayList<>();
         String[] arrayElementsStr = arrayStr.split(" ");
 
         for (String elementStr : arrayElementsStr) {
-            array.add(Integer.parseInt(elementStr));
+            array.add((double) Integer.parseInt(elementStr));
         }
 
         return array;
