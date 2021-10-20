660e3aabb7dad6b90399626a4ee1426aa251fe52
gretzkiy <dias7707@gmail.com>
2021-10-20 19:30:16 +0300

    delet others

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Simplex simplex = new Simplex(2,2);
-        float[][] data = {{1}, {2}};
-        simplex.fillTable(data);
-    }
-
-}
